20985e27352f6ec07a679dd99ce639d3f32ed601
Carlos Rueda <carueda@mbari.org>
2018-01-03 22:40:07 -0800

    minor adjs
    including use of specific mongo version 3.4.2

--- setenv.sh
+++ setenv.sh
@@ -6,14 +6,17 @@ export HOST_CONFIG_DIR=$PWD/config
 # Host ORR data directory
 export ORR_HOST_DATA=$PWD/orr_data
 
-# Host port for the ORR service
+# Host port for the ORR service.
+# Make sure it is an unused port on your host.
 export ORR_HOST_PORT=9090
 
 # Host Mongo data directory
 export MONGO_HOST_DATA=$PWD/mongo_data
 
 # Host port for the Mongo service
+# Make sure it is an unused port on your host.
 export MONGO_HOST_PORT=27017
 
-# Host port for the AGraph service
+# Host port for the AGraph service.
+# Make sure it is an unused port on your host.
 export AGRAPH_HOST_PORT=10035
